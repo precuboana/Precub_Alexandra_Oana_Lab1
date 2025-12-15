d02d83251d3f24974906433d39c8241acedebd3e
ioanrere <ioanrere@gmail.com>
2025-12-15 19:58:57 +0200

    Exercitiu 1

---
+++ Precub_Oana_Lab1/Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Precub_Oana_Lab1.Models;
+
+namespace Precub_Oana_Lab1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Info()
+        {
+            var student = new Student { Name = "Ana", Age = 21 };
+            return View(student);
+        }
+    }
+}
---
+++ Precub_Oana_Lab1/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace Precub_Oana_Lab1.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
