1e0e458bfebc43ae6332dcc45d67a119ea4b3abb
ioanrere <ioanrere@gmail.com>
2025-12-15 19:16:31 +0200

    3 metode in controller

---
+++ Precub_Oana_Lab1/Controllers/PrimulController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Precub_Oana_Lab1.Controllers
+{
+    public class PrimulController : Controller
+    {
+        public string Index()
+        {
+            return "Bun venit pe pagina mea!";
+        }
+
+        public string Salut()
+        {
+            return "Salut";
+        }
+        
+        public string MesajPersonalizat(string nume, int varsta)
+        {
+            return $"Salut {nume}, ai varsta de {varsta} ani.";
+        }
+    }
+}
