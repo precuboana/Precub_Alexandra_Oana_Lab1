4273b95cbf4da2b594412076b0d7a0fa80add3c2
ioanrere <ioanrere@gmail.com>
2025-12-15 20:18:04 +0200

    Exercitiu 3

--- Precub_Oana_Lab1/Program.cs
+++ Precub_Oana_Lab1/Program.cs
@@ -21,6 +21,11 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
+    name: "start",
+    pattern: "start",
+    defaults: new { controller = "Home", action = "Index" });
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
