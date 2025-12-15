ed59acd353b9a474408f90d87d8118d4343f6be1
ioanrere <ioanrere@gmail.com>
2025-12-15 18:38:08 +0200

    Add project files.

---
+++ Precub_Oana_Lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Precub_Oana_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
