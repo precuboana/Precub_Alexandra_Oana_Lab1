b5258138e7508461e8a36b00f3f1eab87448d078
ioanrere <ioanrere@gmail.com>
2025-12-15 20:24:16 +0200

    exercitiu 4

---
+++ Precub_Oana_Lab1/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Precub_Oana_Lab1.Controllers
+{
+    public class DemoController : Controller
+    {
+        [Route("hello")]
+        public string Hello() => "Salut!";
+
+        [HttpGet("hello/{name}")]
+        public string HelloByName(string name) => $"Salut, {name}!";
+
+        [HttpGet("calc/{a:int}/{b:int}")]
+        public string Calculate(int a, int b) => $"Suma este: {a + b}";
+    }
+}
+
